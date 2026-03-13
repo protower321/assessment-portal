21487924e7d5e141a387b0c8ae01e899d634e99d
protower321 <ruanvan651@gmail.com>
2026-03-13 16:28:08 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -8,6 +8,11 @@ row.innerHTML = `
 <td>${app.id}</td>
 <td>${app.name}</td>
 <td>${app.status}</td>
+
+<td>
+<button onclick="viewApplication('${app.id}')">View</button>
+</td>
+
 <td>
 <button onclick="approve('${app.id}')">Approve</button>
 <button onclick="deny('${app.id}')">Deny</button>
@@ -17,6 +22,22 @@ row.innerHTML = `
 table.appendChild(row);
 
 });
+
+function viewApplication(id){
+
+let app = applications.find(a => a.id === id);
+
+document.getElementById("details").classList.remove("hidden");
+
+document.getElementById("d_name").innerText = app.name;
+document.getElementById("d_dob").innerText = app.dob;
+document.getElementById("d_email").innerText = app.email;
+document.getElementById("d_phone").innerText = app.phone;
+document.getElementById("d_address").innerText = app.address;
+document.getElementById("d_length").innerText = app.length;
+document.getElementById("d_reason").innerText = app.reason;
+
+}
 
 function approve(id){
 
