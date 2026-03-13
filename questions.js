3f4bed720cf41e2eed0243eaae25c83e93ba4daa
protower321 <ruanvan651@gmail.com>
2026-03-13 03:04:42 -0400

    Add questions array for quiz functionality

---
+++ questions.js
@@ -0,0 +1,22 @@
+const questions = [
+{
+    question:"What does RCMP stand for?",
+    answers:[
+        "Royal Canadian Mounted Police",
+        "Regional Court Military Police",
+        "Royal Crime Management Patrol",
+        "None"
+    ],
+    correct:0
+},
+{
+    question:"What is a warrant?",
+    answers:[
+        "A legal authorization",
+        "A police badge",
+        "A uniform",
+        "A courthouse"
+    ],
+    correct:0
+}
+];
