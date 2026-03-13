3a173f754106a021afbfbe125b7dc4d62433f5f7
protower321 <ruanvan651@gmail.com>
2026-03-13 03:02:46 -0400

    Refactor register function for cleaner code

--- register.js
+++ register.js
@@ -1,21 +1,17 @@
 function register(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let discord = document.getElementById("discord").value;
+    let roblox = document.getElementById("roblox").value;
 
-let username = document.getElementById("username").value
-let password = document.getElementById("password").value
-let discord = document.getElementById("discord").value
-let roblox = document.getElementById("roblox").value
+    let account = {
+        username: username,
+        password: password,
+        discord: discord,
+        roblox: roblox
+    };
 
-let account = {
-username: username,
-password: password,
-discord: discord,
-roblox: roblox
-}
-
-localStorage.setItem("account", JSON.stringify(account))
-
-alert("Account successfully created!")
-
-window.location.href = "index.html"
-
+    localStorage.setItem("account", JSON.stringify(account));
+    alert("Account successfully created!");
+    window.location.href = "index.html";
 }
