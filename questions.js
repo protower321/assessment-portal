735d7f6c1244b3432d0af976ebabaa8d4c65e41a
protower321 <ruanvan651@gmail.com>
2026-03-13 03:14:22 -0400

    Refactor questions by removing 'correct' property
    Removed the 'correct' property from each question object.

--- questions.js
+++ questions.js
@@ -1,22 +1,20 @@
 const questions = [
-{
-    question:"What does RCMP stand for?",
-    answers:[
-        "Royal Canadian Mounted Police",
-        "Regional Court Military Police",
-        "Royal Crime Management Patrol",
-        "None"
-    ],
-    correct:0
-},
-{
-    question:"What is a warrant?",
-    answers:[
-        "A legal authorization",
-        "A police badge",
-        "A uniform",
-        "A courthouse"
-    ],
-    correct:0
-}
+  {
+    question: "What does RCMP stand for?",
+    answers: [
+      "Royal Canadian Mounted Police",
+      "Regional Court Military Police",
+      "Royal Crime Management Patrol",
+      "None"
+    ]
+  },
+  {
+    question: "What is a warrant?",
+    answers: [
+      "A legal authorization",
+      "A police badge",
+      "A uniform",
+      "A courthouse"
+    ]
+  }
 ];
